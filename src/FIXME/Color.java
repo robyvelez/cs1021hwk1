8fb14e4d22e80cc2ca337f03636fe7b6732da238
velez <velez@msoe.edu>
2021-11-28 20:36:15 -0600

    Minor fixes. Add stub for Color.java

---
+++ src/FIXME/Color.java
@@ -0,0 +1,4 @@
+package FIXME;
+
+public enum Color {
+}
--- src/FIXME/ColorDriver.java
+++ src/FIXME/ColorDriver.java
@@ -1,10 +1,4 @@
-package FIXME;/*
- * CS 1011 - Section 061/081
- * Fall 2021
- * Lecture Demo
- * Name: Roby Velez
- * Created: 11/28/2021
- */
+package FIXME;
 
 public class ColorDriver {
 }
